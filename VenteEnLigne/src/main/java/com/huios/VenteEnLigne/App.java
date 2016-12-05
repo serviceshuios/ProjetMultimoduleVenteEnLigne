da08f469d844787e7c88980e2da7efb7a48f05bb
HUIOS <eddy.zam@gmail.com>
2016-12-05 15:26:16 +0100

    structure initiale du projet

---
+++ VenteEnLigne/src/main/java/com/huios/VenteEnLigne/App.java
@@ -0,0 +1,13 @@
+package com.huios.VenteEnLigne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
