ec98805e846198477a4552abe095f2eb01284e0c
HUIOS <eddy.zam@gmail.com>
2016-12-06 16:34:19 +0100

    Ajout des Classes Role et User

---
+++ VenteEnLigne/src/main/java/com/huios/VenteEnLigne/metier/Role.java
@@ -0,0 +1,28 @@
+package com.huios.VenteEnLigne.metier;
+
+import java.util.Collection;
+
+public class Role {
+	private Long idRole;
+	private String nomRole;
+	private Collection<User> users;
+	public Long getIdRole() {
+		return idRole;
+	}
+	public void setIdRole(Long idRole) {
+		this.idRole = idRole;
+	}
+	public String getNomRole() {
+		return nomRole;
+	}
+	public void setNomRole(String nomRole) {
+		this.nomRole = nomRole;
+	}
+	public Collection<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Collection<User> users) {
+		this.users = users;
+	}
+	
+}
