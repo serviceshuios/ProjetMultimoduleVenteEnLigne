f83ccaacf601f242d98586609ecf587e784c1e50
serviceshuios <services.huios@gmail.com>
2018-12-05 14:03:02 +0100

    mise à jour couche service

--- VenteEnLigne/monappli-service/src/main/java/com/huios/service/ServiceClient.java
+++ VenteEnLigne/monappli-service/src/main/java/com/huios/service/ServiceClient.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.huios.VenteEnLigne.dao.UserDao;
 import com.huios.VenteEnlLigne.metier.User;
 
+@Service
 public class ServiceClient implements IServiceClient {
 
 	@Autowired
